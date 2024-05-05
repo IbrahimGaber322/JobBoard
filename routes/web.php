cd981649be21457b5dffbd3ec25758b9c8036cfe
IbrahimGaber322 <ibrahimseda322@gmail.com>
2024-05-05 20:47:15 +0300

    email_verification_added

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('verified')->group(function () {
     Route::get('/demo', [DemoController::class, 'index'])->name('demo');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
