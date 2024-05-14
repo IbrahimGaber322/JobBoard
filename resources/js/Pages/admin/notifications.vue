9d797092c7cfdd5deea70bd687128ba63c8f58f2
shimaaFathi <shimaafathi126@gmail.com>
2024-05-15 00:47:56 +0300

    style-notification-page

--- resources/js/Pages/admin/notifications.vue
+++ resources/js/Pages/admin/notifications.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <h2>Notifications</h2>
+    <h2 class="text-xl font-bold mb-4">Notifications</h2>
     <ul>
-      <li v-for="(notification, index) in notifications" :key="index">
-        {{ notification }}
+      <li v-for="(notification, index) in notifications" :key="index" class="mb-2">
+        <div class="flex items-center justify-between border rounded-lg p-4 text-sm bg-blue-100">
+          <div class="flex items-center">
+            <div class="w-4 h-4 bg-blue-500 rounded-full mr-2"></div> <!-- Colored dot -->
+            <span class="font-semibold">Notification:</span>
+            <span class="ml-2">{{ notification }}</span>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
