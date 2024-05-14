a634f84541cd93fd8020958378537aa9bcc45549
shimaaFathi <shimaafathi126@gmail.com>
2024-05-14 15:24:44 +0300

    style-accepted-jobs

--- resources/js/Pages/admin/accepted-job-postings.vue
+++ resources/js/Pages/admin/accepted-job-postings.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="container mx-auto">
-        <h1 class="text-3xl font-bold mb-4">Accepted Job Postings</h1>
+        <h1 class="text-3xl font-bold mb-8">Accepted Job Postings</h1>
         
         <!-- Tailwind CSS cards to display accepted job postings -->
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <div v-for="acceptedJob in acceptedJobPostings" :key="acceptedJob.id">
-                <div class="bg-white rounded-lg shadow-md p-6">
-                    <h2 class="text-xl font-semibold mb-4">{{ acceptedJob.title }}</h2>
-                    <p class="text-gray-700 mb-4">{{ acceptedJob.desc }}</p>
-                    <p class="text-gray-700 mb-4">Category: {{ acceptedJob.category }}</p>
-                    <p class="text-gray-700 mb-4">Location: {{ acceptedJob.location }}</p>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            <div v-for="acceptedJob in acceptedJobPostings" :key="acceptedJob.id" class="relative">
+                <div class="bg-green-100 rounded-lg shadow-md p-4">
+                    <h2 class="text-lg font-semibold mb-2 text-green-800">{{ acceptedJob.title }}</h2>
+                    <p class="text-gray-700 mb-2">{{ acceptedJob.desc }}</p>
+                    <div class="text-gray-700 mb-2">
+                        <p class="font-semibold text-green-800">Category:</p>
+                        <p class="italic">{{ acceptedJob.category }}</p>
+                        <p class="font-semibold text-green-800">Location:</p>
+                        <p class="italic">{{ acceptedJob.location }}</p>
+                    </div>
                     <!-- Add additional job details as needed -->
                 </div>
             </div>
