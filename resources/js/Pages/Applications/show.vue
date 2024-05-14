e659d3a1c779e8044e727577b915bc08e6e71559
Salma158 <salmasherif856@gmail.com>
2024-05-14 11:11:35 -0700

    added switching between application status

--- resources/js/Pages/Applications/show.vue
+++ resources/js/Pages/Applications/show.vue
@@ -29,15 +29,16 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
                         </div>
                         <div class="flex justify-between items-center">
                             <button @click="markStatus('Rejected', application.id)" :class="{
-                                'text-red-600 hover:underline': application.status === 'pending',
-                                'text-gray-400 cursor-not-allowed': application.status !== 'pending'
-                            }" :disabled="application.status !== 'pending'">
+                                'text-red-600 hover:underline': application.status === 'pending' || application.status === 'Accepted',
+                                'text-gray-400 cursor-not-allowed': application.status === 'Rejected'
+                            }" :disabled="application.status === 'Rejected'">
                                 Mark Rejected
                             </button>
+
                             <button @click="markStatus('Accepted', application.id)" :class="{
-                                'text-blue-600 hover:underline': application.status === 'pending',
-                                'text-gray-400 cursor-not-allowed': application.status !== 'pending'
-                            }" :disabled="application.status !== 'pending'">
+                                'text-red-600 hover:underline': application.status === 'pending' || application.status === 'Rejected',
+                                'text-gray-400 cursor-not-allowed': application.status === 'Accepted'
+                            }" :disabled="application.status === 'Accepted'">
                                 Mark Accepted
                             </button>
                         </div>
@@ -49,7 +50,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 </template>
 
 
-    <script>
+<script>
 export default {
     props: {
         userApplications: {
