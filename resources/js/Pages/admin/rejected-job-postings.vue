9b76eca36044bb3586a2bbe1ca245701e4215109
shimaaFathi <shimaafathi126@gmail.com>
2024-05-14 15:26:58 +0300

    style-accepted-jobs

--- resources/js/Pages/admin/rejected-job-postings.vue
+++ resources/js/Pages/admin/rejected-job-postings.vue
@@ -5,11 +5,15 @@
         <!-- Tailwind CSS cards to display rejected job postings -->
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             <div v-for="rejectedJob in rejectedJobPostings" :key="rejectedJob.id">
-                <div class="bg-white rounded-lg shadow-md p-6">
-                    <h2 class="text-xl font-semibold mb-4">{{ rejectedJob.title }}</h2>
-                    <p class="text-gray-700 mb-4">{{ rejectedJob.desc }}</p>
-                    <p class="text-gray-700 mb-4">Category: {{ rejectedJob.category }}</p>
-                    <p class="text-gray-700 mb-4">Location: {{ rejectedJob.location }}</p>
+                <div class="bg-red-100 rounded-lg shadow-md p-4">
+                    <h2 class="text-lg font-semibold mb-2 text-red-800">{{ rejectedJob.title }}</h2>
+                    <p class="text-gray-700 mb-2">{{ rejectedJob.desc }}</p>
+                    <div class="text-gray-700 mb-2">
+                        <p class="font-semibold text-red-800">Category:</p>
+                        <p class="italic">{{ rejectedJob.category }}</p>
+                        <p class="font-semibold text-red-800">Location:</p>
+                        <p class="italic">{{ rejectedJob.location }}</p>
+                    </div>
                     <!-- Add additional job details as needed -->
                 </div>
             </div>
