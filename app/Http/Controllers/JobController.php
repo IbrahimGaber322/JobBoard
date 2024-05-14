09062c425621f658181d43ffe23e32f5880dd5a0
omniagad25 <omniagad25@gmail.com>
2024-05-15 00:04:51 +0300

    adding admin controller

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -49,9 +49,9 @@ class JobController extends Controller
             'image' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
         ]);
   // Inside the store method
-$employerName = auth()->user()->name; // Assuming the employer's name is stored in the 'name' field
-$admin = User::where('role', 'admin')->first();
-$admin->notify(new NewJobAddedNotification($employerName));
+        $employerName = auth()->user()->name; // Assuming the employer's name is stored in the 'name' field
+        $admin = User::where('role', 'admin')->first();
+        $admin->notify(new NewJobAddedNotification($employerName));
         $userId = auth()->id(); 
         if ($request->hasFile('image')) {
             $image = $request->file('image');
