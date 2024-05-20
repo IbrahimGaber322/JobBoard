df11cd77e4450a453645340a8c73d6568f93c41f
shimaaFathi <shimaafathi126@gmail.com>
2024-05-20 22:08:03 +0300

    restyle-candidates-page

--- resources/js/Pages/admin/candidate-applications.vue
+++ resources/js/Pages/admin/candidate-applications.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="container mx-auto">
-    <h1 class="text-3xl font-bold mb-4">Candidate Applications</h1>
+  <div>
+      <Navbar/>
+      <div class="relative bg-gradient-to-r">
+
+        <div class="absolute bg-blue-600 rounded-full w-48 h-48 -top-16 -left-16 mix-blend-multiply opacity-50"></div>
+          <div class="absolute bg-blue-500 w-64 h-64 transform rotate-45 -bottom-16 -right-32 mix-blend-multiply opacity-50"></div>
+          <div class="absolute bg-blue-700 w-96 h-32 -bottom-8 -right-16 mix-blend-multiply opacity-50"></div>
+
+          <div class="container mx-auto">
+              <div class="flex justify-center">
+                  <h1 class="text-4xl font-bold mt-5 text-blue-900">Candidates</h1>
+              </div>
 
     <div v-if="candidates.length === 0">
       <p>No candidates found.</p>
@@ -15,12 +25,18 @@
       </div>
     </div>
   </div>
+  </div>
+  </div>
 </template>
 
 <script>
 import Chart from 'chart.js/auto';
+import Navbar from './Navbar.vue';
 
 export default {
+  components:{
+Navbar
+  },
   props: {
     candidates: Array,
   },
--- resources/js/Pages/admin/employee-job-statistics.vue
+++ resources/js/Pages/admin/employee-job-statistics.vue
@@ -10,7 +10,7 @@
           <div class="container mx-auto">
               <div class="flex justify-center">
                   <h1 class="text-4xl font-bold mt-5 text-blue-900">Employers</h1>
-              </div>>
+              </div>
 
     <div v-if="employees.length === 0">
       <p>No employees found.</p>
