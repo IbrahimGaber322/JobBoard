dad55cabad478e4f95fabb3bf9750d773d28361c
shimaaFathi <shimaafathi126@gmail.com>
2024-05-22 01:25:29 +0300

    update-redirect-to-post-detail

--- resources/js/Pages/admin/job-postings.vue
+++ resources/js/Pages/admin/job-postings.vue
@@ -14,20 +14,22 @@
                 <div class="flex justify-center items-center">
                     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-10 mb-10">
                         <div v-for="pendingJob in pendingJobPostings" :key="pendingJob.id" class="relative">
-                            <div class="bg-white rounded-lg shadow-md p-6 w-full md:w-5/6 lg:w-8/10 jobCard">
-                                <h2 class="text-lg font-semibold mb-4 text-blue-900">{{ pendingJob.title }}</h2>
-                                <p class="text-gray-700 mb-4 description">{{ pendingJob.desc }}</p>
-                                <div class="text-gray-700 mb-4">
-                                    <p class="font-semibold text-blue-900">Category:</p>
-                                    <p class="italic">{{ pendingJob.category }}</p>
-                                    <p class="font-semibold text-blue-900">Location:</p>
-                                    <p class="italic">{{ pendingJob.location }}</p>
+                            <a :href="'/admin/job-postings/' + pendingJob.id">
+                                <div class="bg-white rounded-lg shadow-md p-6 w-full md:w-5/6 lg:w-8/10 jobCard cursor-pointer">
+                                    <h2 class="text-lg font-semibold mb-4 text-blue-900">{{ pendingJob.title }}</h2>
+                                    <p class="text-gray-700 mb-4 description">{{ pendingJob.desc }}</p>
+                                    <div class="text-gray-700 mb-4">
+                                        <p class="font-semibold text-blue-900">Category:</p>
+                                        <p class="italic">{{ pendingJob.category }}</p>
+                                        <p class="font-semibold text-blue-900">Location:</p>
+                                        <p class="italic">{{ pendingJob.location }}</p>
+                                    </div>
+                                    <div class="flex justify-between items-center">
+                                        <button @click="approveJob(pendingJob.id)" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded focus:outline-none">Approve</button>
+                                        <button @click="rejectJob(pendingJob.id)" class="bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded focus:outline-none">Reject</button>
+                                    </div>
                                 </div>
-                                <div class="flex justify-between items-center">
-                                    <button @click="approveJob(pendingJob.id)" class="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded focus:outline-none">Approve</button>
-                                    <button @click="rejectJob(pendingJob.id)" class="bg-gray-200 hover:bg-gray-300 text-gray-800 px-4 py-2 rounded focus:outline-none">Reject</button>
-                                </div>
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -35,7 +37,6 @@
         </div>
     </div>
 </template>
-
 <script>
 import Navbar from './Navbar.vue';
 
